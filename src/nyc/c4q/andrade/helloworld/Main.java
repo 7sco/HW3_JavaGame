dcbca4bd80120c475f3aa2610f4ed16a86550bdd
Francisco Andrade <xaviandrade14@gmail.com>
2017-10-06 00:13:53 -0400

    Questions Added/ Missing Sorting to Java program-HW3

--- src/nyc/c4q/andrade/helloworld/Main.java
+++ src/nyc/c4q/andrade/helloworld/Main.java
@@ -11,8 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-           // menu();
+      //  menu();
 
 
         quiz();
@@ -68,23 +67,6 @@ public class Main {
         System.out.println("Hi, welcome to java-quiz 1.0. Answer the following 5 questions with numerical input for a total score!");
         System.out.println();
 
-//        Quiz quiz[]= {
-//                new Quiz("A parameter is: ", "The input of a method"),
-//
-//                new Quiz("Which of the following statements is false about objects?", "Objects do not permit encapsulation"),
-//
-//                new Quiz("Which methods can access to private attributes of a class?", "Only methods those defined in the same class"),
-//
-//                new Quiz("Which of the following is not a return type?", "public"),
-//
-//                new Quiz("If result = 2 + 3 * 5, what is the value and type of ‘result’ variable?", "17, int")
-//        } ;
-//
-//                for(int i=0 ; i<quiz.length; i++ ){
-//                    //System.out.println( "Quest: "+quiz[i].getQuestion()+"\nAnsw: "+quiz[i].getAnswer());
-//                }
-
-
 
                 ArrayList<Quiz> arr = new ArrayList<Quiz>( Arrays.asList(new Quiz("A parameter is: ", "The input of a method"),
 
@@ -96,13 +78,44 @@ public class Main {
 
                         new Quiz("If result = 2 + 3 * 5, what is the value and type of ‘result’ variable?", "17, int")));
 
+        System.out.println("Make the right choice to get points: \n");
         for (int i=0; i<arr.size(); i++) {
            // System.out.println(arr.get(i).getQuestion()+"\n"+arr.get(i).getAnswer());
 
             Quiz q=new Quiz(arr.get(i).getQuestion(), arr.get(i).getAnswer());
-            q.questionOptions(q.getQuestion(),q.getAnswer());
+
+            System.out.println(q.getQuestion());
+
+
+            System.out.println();
+            ArrayList<String> strg= q.questionOptions((i+1));
+
+            System.out.println("1. "+strg.get(0));
+            System.out.println("2. "+q.getAnswer());
+            System.out.println("3. "+strg.get(1));
+            //System.out.println(q.questionOptions((i+1)));
+            System.out.print("Enter your Choice: ");
+            int ans = input.nextInt();
+
+
+
+            while(ans!=1){
+                System.out.print("Try again,Enter your Choice: ");
+                ans = input.nextInt();
+                if(ans==1){
+
+                    System.out.println("Great you got it right, you get 10pts");
+
+                    break;
+                }
+
+            }
+            System.out.println();
 
         }
+
+
+        //sorting answer order and getting theright naswer missing
 
 
 
@@ -116,7 +129,6 @@ public class Main {
       String result= java.getAnswer(java.getTerms());
       System.out.println(result);
 
-      //return result;
     }
 
 
--- src/nyc/c4q/andrade/helloworld/Quiz.java
+++ src/nyc/c4q/andrade/helloworld/Quiz.java
@@ -33,68 +33,105 @@ public class Quiz {
 
 
 
-    public void questionOptions(String question, String answer) {
+    public ArrayList<String> questionOptions(int num) {
 
+
+    if (num==1){
     ArrayList<String> optionsQ1 = new ArrayList<String>(Arrays.asList("a method call","a subclass of string" ));
+    return optionsQ1;
+    }
 
+    else if(num==2){
     ArrayList<String> optionsQ2 = new ArrayList<String>(Arrays.asList("An instance of a class is an object","Objects can access both static and instance data", "Object is the super class of all other classes" ));
+        return optionsQ2;
+    }
 
+    else if (num==3){
     ArrayList<String> optionsQ3 = new ArrayList<String>(Arrays.asList("Only Static methods of the same class","Only instances of the same class","Only classes available in the same package." ));
+        return optionsQ3;
+    }
 
+    else if (num==4){
     ArrayList<String> optionsQ4 = new ArrayList<String>(Arrays.asList("boolean","void","Button" ));
+        return optionsQ4;
+    }
 
+    else{
 
     ArrayList<String> optionsQ5 = new ArrayList<String>(Arrays.asList("17, byte","25, byte","25, int" ));
-
-    if (answer.equals("The input of a method")){
-        optionsQ1.add(answer);
-        System.out.println(question);
-        for (int i=0; i<optionsQ1.size(); i++)  {
-            System.out.println(optionsQ1.get(i));
+        return optionsQ5;
         }
-        System.out.println();
-    }
-
-    if (answer.equals("Objects do not permit encapsulation")){
-        optionsQ2.add(answer);
-        System.out.println(question);
-        for (int i=0; i<optionsQ2.size(); i++)  {
-            System.out.println(optionsQ2.get(i));
-
-        }
-        System.out.println();
-    }
 
 
-    if (answer.equals("Only methods those defined in the same class")){
-        optionsQ3.add(answer);
-        System.out.println(question);
-        for (int i=0; i<optionsQ3.size(); i++)  {
-            System.out.println(optionsQ3.get(i));
 
-        }
-        System.out.println();
-    }
 
-    if (answer.equals("public")){
-        optionsQ4.add(answer);
-        System.out.println(question);
-        for (int i=0; i<optionsQ4.size(); i++)  {
-            System.out.println(optionsQ4.get(i));
 
-        }
-        System.out.println();
-    }
 
-    if (answer.equals("17, int")){
-        optionsQ5.add(answer);
-        System.out.println(question);
-        for (int i=0; i<optionsQ5.size(); i++)  {
-            System.out.println(optionsQ5.get(i));
 
-        }
-        System.out.println();
-    }
+
+
+
+
+//    if (answer.equals("The input of a method")){
+//        optionsQ1.add(answer);
+//        System.out.println(question);
+//        for (int i=0; i<optionsQ1.size(); i++)  {
+//            System.out.println(optionsQ1.get(i));
+//        }
+//
+//        return optionsQ1;
+//        //System.out.println();
+//    }
+//
+//    if (answer.equals("Objects do not permit encapsulation")){
+//        optionsQ2.add(answer);
+//        System.out.println(question);
+//        for (int i=0; i<optionsQ2.size(); i++)  {
+//            System.out.println(optionsQ2.get(i));
+//
+//        }
+//        System.out.println();
+//        return optionsQ2;
+//    }
+//
+//
+//    if (answer.equals("Only methods those defined in the same class")){
+//        optionsQ3.add(answer);
+//        System.out.println(question);
+//        for (int i=0; i<optionsQ3.size(); i++)  {
+//            System.out.println(optionsQ3.get(i));
+//
+//        }
+//        System.out.println();
+//        return optionsQ3;
+//    }
+//
+//    if (answer.equals("public")){
+//        optionsQ4.add(answer);
+//        System.out.println(question);
+//        for (int i=0; i<optionsQ4.size(); i++)  {
+//            System.out.println(optionsQ4.get(i));
+//
+//        }
+//        System.out.println();
+//        return optionsQ4;
+//    }
+//
+//    else if (answer.equals("17, int")){
+//        optionsQ5.add(answer);
+//        System.out.println(question);
+//        for (int i=0; i<optionsQ5.size(); i++)  {
+//            System.out.println(optionsQ5.get(i));
+//
+//        }
+//        System.out.println();
+//
+//        return optionsQ5;
+//    }
+//
+//        return optionsQ1;
+
+
 
 
     }
